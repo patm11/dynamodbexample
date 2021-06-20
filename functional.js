3b0d02518aa4eadd4627ce3651f2c5ae93d0260a
patm11 <patrick.t.may@gmail.com>
2021-06-20 00:14:56 -0500

    branch1 Added functional test that ensures the application is up

--- functional.js
+++ functional.js
@@ -1,33 +1,45 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
+async function startDockerApp () {
+  const { stdout, stderr } = await exec("yarn start:docker");
+  console.info(stdout);
+  console.info(stderr);
+}
+
+async function runFunctionalTests () {
+  console.info("Intentional pause to allow docker-compose to complete");
+  await new Promise(resolve => { setTimeout(resolve, 3000); });
+  const { stdout, stderr } = await exec("yarn test:functional");
+  console.info(stdout);
+  console.info(stderr);
+}
+
+async function stopDockerApplication () {
+  console.info("Shutting down docker compose...");
+  const { stdout, stderr } = await exec("yarn stop:docker");
+  console.info(stdout);
+  console.info(stderr);
+}
+
 async function functionalTestHandler () {
   try {
-    // start the app and run the functional tests
-    const startDockerOutput = await exec("yarn start:docker");
-    console.info(startDockerOutput);
-    console.info("Intentional pause to allow docker-compose to complete");
-    await new Promise(resolve => { setTimeout(resolve, 3000); });
-    const functionalTestOutput = await exec("yarn test:functional");
-    console.info(functionalTestOutput);
+    // start the app
+    await startDockerApp();
+    // run the functional tests
+    await runFunctionalTests();
   } catch (error) {
     console.log(`Error during functional test exec: ${JSON.stringify(error)}`);
   } finally {
-    console.info("Shutting down docker compose...");
-    const { stdout, stderr } = await exec("yarn stop:docker");
-
-    if (stderr) {
-      console.error(JSON.stringify(stderr));
-    } else {
-      console.info(JSON.stringify(stdout));
-    }
+    // shut down the application after tests have run
+    await stopDockerApplication();
   }
 }
 
 functionalTestHandler()
   .then(() => {
-    console.info("Successful functional test run");
+    console.info("End functional test script");
   })
   .catch(() => {
-    console.error("Unsuccessful functional test run");
+    console.error("Functional test script ran unsuccessfully");
   });
--- functional/exampleFunctional.test.ts
+++ functional/exampleFunctional.test.ts
@@ -1,9 +1,18 @@
+import axios from "axios";
+
 describe("Functional tests for the dynamodb example web service", () => {
-  // eslint-disable-next-line no-unused-vars
   const BASE_URL = "http://localhost:3000";
 
-  it("Faux test", () => {
-    // eslint-disable-next-line no-self-compare
-    expect(1 === 1).toBeTruthy();
+  const client = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  it("Can detect the app heartbeat", async () => {
+    const response = await client.get("/");
+
+    expect(response.status).toEqual(200);
   });
 });
